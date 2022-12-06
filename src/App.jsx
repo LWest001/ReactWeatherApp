5b284b5233e61ecf231f1c21068d389429e57f51
Leo W <leo.westebbe@gmail.com>
2022-12-06 14:28:45 -0500

    State setters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ import { getCoordinates, getLocalWeatherData } from "./API/OpenWeather";
 import { getLocationString } from "./API/ZipCodeBase";
 
 function App() {
+  // State setters
+  const [toggleView, setToggleView] = useState("LocationForm");
+  const [postalCode, setPostalCode] = useState("");
+  const [countryCode, setCountryCode] = useState("US");
+  const [country, setCountry] = useState("United States");
+  const [units, setUnits] = useState("");
+  const [locationString, setLocationString] = useState("Loading...");
+  const [coordinates, setCoordinates] = useState("");
+  const [localWeatherData, setLocalWeatherData] = useState({});
+  const [isValidPostalCode, setIsValidPostalCode] = useState(false);
   return (
     <div className="App">
       <MainDiv />
