7332eda3dfb5ec28621476d8744cc31c125cd3c2
Leo W <leo.westebbe@gmail.com>
2022-12-14 12:59:05 -0500

    repair grid issue from Weather and Temp cells

--- src/Container/Result/Result.jsx
+++ src/Container/Result/Result.jsx
@@ -3,12 +3,11 @@ import "./Result.css";
 export const Result = (props) => {
   const { display, icon, styleDisplay } = props;
   const { heading, data } = display;
-  const bgImage = heading === "Temperature" ? icon : "";
   const iconImg = heading === "Temperature" ? <img id="weatherIcon" src={icon} alt="icon of current weather"/> : "";
   return (
     <div
       className={`Result ${heading}`}
-      style={{ backgroundImage: `url(${bgImage})`, display: styleDisplay }}
+      style={{ display: styleDisplay }}
     >
       <h4 className="heading">{heading}</h4>
       <p className="data">{data}</p>
