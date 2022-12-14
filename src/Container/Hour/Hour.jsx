d23d02d47cc2f0e358f3e35149ba873827a32a9e
Leo W <leo.westebbe@gmail.com>
2022-12-14 16:29:35 -0500

    set styling for Hourly elements

--- src/Container/Hour/Hour.jsx
+++ src/Container/Hour/Hour.jsx
@@ -1,18 +1,28 @@
 import "./Hour.css";
 export const Hour = (props) => {
   const { time, date, currentDate, icon, temp, weather, dataType } = props;
-  const bgColor =
-    date !== currentDate
-      ? {
+  const bgColor = () => {
+    if (date !== currentDate) {
+      if (dataType !== "Hourly") {
+        return {
           background:
             "linear-gradient(0deg, rgba(2,0,36,0) 0%, rgba(255,255,0,0.05) 5%, rgba(255,255,0,0.3) 100%)",
-        }
-      : { backgroundColor: "" };
+        };
+      }
+      return {
+        background:
+            "linear-gradient(270deg, rgba(2,0,36,0) 0%, rgba(255,255,0,0.05) 5%, rgba(255,255,0,0.3) 100%)",
+      }
+    } else {
+      return { backgroundColor: "" };
+    }
+  };
   return (
-    <div className={"Hour " + dataType} style={bgColor}>
+    <div className={"Hour " + dataType} style={bgColor()}>
       <h4>{time}</h4>
       <img src={icon} alt={weather} />
       <p>{temp}</p>
+      <p>{weather}</p>
     </div>
   );
 };
