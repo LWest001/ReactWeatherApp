82e4536ed66389cdd596c94a89c6ea3593967285
Leo W <leo.westebbe@gmail.com>
2023-02-10 17:54:55 -0500

    simplify HourlyDisplay

--- src/Container/HourlyDisplay/HourlyDisplay.jsx
+++ src/Container/HourlyDisplay/HourlyDisplay.jsx
@@ -6,26 +6,24 @@ import { selectWeatherData, selectDataView } from "../../app/appSlice";
 export const HourlyDisplay = ({ dates }) => {
   const { hourlyData } = useSelector(selectWeatherData);
   const dataView = useSelector(selectDataView);
-  const HoursGrid = () => {
-    let hoursArray = [];
-    hourlyData.forEach((hour, i) => {
-      hoursArray.push(
-        <Hour
-          key={hour.text.Time + hour.text.Date}
-          index={i}
-          date={hour.text.Date}
-          dates={dates}
-        />
-      );
-    });
-    return hoursArray;
-  };
+
+  let hoursArray = hourlyData.map((hour, i) => {
+    return (
+      <Hour
+        key={hour.text.Time + hour.text.Date}
+        index={i}
+        date={hour.text.Date}
+        dates={dates}
+      />
+    );
+  });
+
   return (
     <div
       className="HourlyDisplay"
       style={dataView === "Hourly" ? { gridTemplateColumns: "1fr" } : {}}
     >
-      <HoursGrid />
+      {hoursArray}
     </div>
   );
 };
