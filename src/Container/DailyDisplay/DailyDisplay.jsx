c107e0322e5e11106881408bb7c1042ad708c0f8
Leo W <leo.westebbe@gmail.com>
2023-02-10 17:55:48 -0500

    simplify DailyDisplay with .map function

--- src/Container/DailyDisplay/DailyDisplay.jsx
+++ src/Container/DailyDisplay/DailyDisplay.jsx
@@ -2,9 +2,8 @@ import "./DailyDisplay.css";
 import { Day } from "./Day/Day";
 
 export const DailyDisplay = ({ dailyData }) => {
-  let daysArray = [];
-  dailyData.forEach((day, i) => {
-    daysArray.push(<Day key={day.text.Date} index={i} />);
+  let daysArray = dailyData.map((day, i) => {
+    return <Day key={day.text.Date} index={i} />;
   });
 
   return <div className="DailyDisplay">{daysArray}</div>;
